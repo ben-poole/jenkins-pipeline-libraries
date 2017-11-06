b41e69d1dfb4d7f16412f977d50019cf35c92cc7
Ben Poole <ben.poole@aerian.com>
2017-11-06 13:43:36 +0000

    debug

--- vars/generateAwsCreds.groovy
+++ vars/generateAwsCreds.groovy
@@ -1,10 +1,8 @@
 def call(mfaArn, totpKey) {
-    //sh """
-	    sh "echo 'Generating 2FA code'"
-      sh "oathtool --totp -b ${totpKey}"
-      //code=`oathtool --totp -b ${totpKey}`
+  //sh "echo 'Generating 2FA code'"
+  //sh "oathtool --totp -b ${totpKey}"
+  //code=`oathtool --totp -b ${totpKey}`
 
-      //echo 'Generating token'
-      //aws sts get-session-token --serial-number ${mfaArn} --token-code $code 
-	//"""
+  echo 'Generating token'
+  sh "aws sts get-session-token --serial-number ${mfaArn} --token-code `oathtool --totp -b ${totpKey}`" 
 }
